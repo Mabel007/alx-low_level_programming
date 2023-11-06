6de662d0faf410ee6f878ce39e2582c7e95967fe
Christiana <chrisbarbz17@gmail.com>
2023-11-06 12:19:16 +0000

    More Malloc

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _calloc - ...
+ * @nmemb: number of members
+ * @size: the size
+ *
+ * Return: the return statement
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+
+	int i = 0, l = 0;
+	char *p;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	l = nmemb * size;
+	p = malloc(l);
+
+	if (p == NULL)
+		return (NULL);
+
+	while (i < l)
+	{
+		p[i] = 0;
+		i++;
+
+	}
+
+	return (p);
+
+}
