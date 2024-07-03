90e244cd4b83125ada23561669581e340e45412c
Christiana <chrisbarbz17@gmail.com>
2024-07-03 18:37:16 +0000

    Modify island perimeter file

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 
 def island_perimeter(grid):
-    # returns the perimeter of the island
-    # Grid represents water by zero and land by one
-
+    """
+    Returns the perimeter of the island.
+    Grid represents water by zero and land by one.
+    """
     width = len(grid[0])
     height = len(grid)
     edges = 0
@@ -18,5 +19,6 @@ def island_perimeter(grid):
                 if (i > 0 and grid[i - 1][j] == 1):
                     edges += 1
 
-    return size * 4 - edges * 2
+    return ((size * 4) - (edges * 2))
+
 
